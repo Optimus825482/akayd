3d49fa7c4c23b6de68c1cd5439dd141afb464381
optmus825454 <ikinciyenikitap54@gmail.com>
2026-07-31 03:52:33 +0300

    update: ContactPage.tsx, FindikIslemePage.tsx, FindikIslemePage_orig.tsx

--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -180,7 +180,7 @@ const ContactPage: React.FC<ContactPageProps> = ({ content, seoSettings }) => {
                         <div dangerouslySetInnerHTML={{__html:content.map_embed}} className="w-full h-full" />
                     ) : (
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023!2d30.748!3d40.7987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQ3JzU1LjMiTiAzMMKwNDQnNTIuOCJF!5e0!3m2!1str!2str!4v1620000000000"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12091!2d30.7521!3d40.7885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ccaf7eb5d0a2d5%3A0x0!2zNDDCsDQ3JzE4LjYiTiAzMMKwNDUnMDcuNiJF!5e0!3m2!1str!2str!4v1690000000000"
                             width="100%" height="450"
                             style={{border:0}}
                             allowFullScreen
